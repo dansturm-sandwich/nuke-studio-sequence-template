6ec0814c1ee24104ad264698277bb67a7bfa44a5
Dan Sturm <dan@sandwich.co>
2024-05-23 21:40:56 -0700

    initial commit

---
+++ ns_sequence_template.py
@@ -0,0 +1,50 @@
+import hiero
+
+
+def sequence_template_tracks():
+	sequence = hiero.ui.activeSequence()
+	plates_track_1 = hiero.core.VideoTrack("plates 1")
+	plates_track_2 = hiero.core.VideoTrack("plates 2")
+	comps_track_1 = hiero.core.VideoTrack("comps 1")
+	comps_track_2 = hiero.core.VideoTrack("comps 2")
+	slate_track = hiero.core.VideoTrack("slate")
+	spacer_track = hiero.core.VideoTrack("--------")
+	sequence.addTrack(spacer_track)
+	sequence.addTrack(plates_track_1)
+	sequence.addTrack(plates_track_2)
+	sequence.addTrack(spacer_track)
+	sequence.addTrack(comps_track_1)
+	sequence.addTrack(comps_track_2)
+	sequence.addTrack(spacer_track)
+	sequence.addTrack(slate_track)
+
+
+
+def remove_all_audio_tracks():
+	sequence = hiero.ui.activeSequence()
+	audio_tracks = [track for track in sequence.audioTracks()]
+	for audio_track in audio_tracks:
+    	sequence.removeTrack(audio_track)
+
+    audio_track = hiero.core.AudioTrack("Audio 1")
+	sequence.addTrack(audio_track)
+
+
+
+
+# def add_slate_effect():
+# 	sequence = hiero.ui.activeSequence()
+
+# 	video_track = sequence.videoTracks()[0]
+
+# 	text_effect = hiero.core.EffectTrackItem('Text', 'net.fx.text')
+
+# 	# Set the parameters for the text effect
+# 	text_effect.node().knob('message').setValue('Hello World')
+
+# 	# Add the text effect to the video track
+# 	video_track.addTrackItem(text_effect)
+
+# 	# Set the in and out times for the text effect
+# 	text_effect.setTimelineIn(0)
+# 	text_effect.setTimelineOut(sequence.duration() - 1)
